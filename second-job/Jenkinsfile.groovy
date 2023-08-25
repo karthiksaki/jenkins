e3ff95132e5f2c7da708030ca7cd9133007e6b4e
Karthik <124011389+karthiksaki@users.noreply.github.com>
2023-08-24 21:19:29 -0500

    Update Jenkinsfile.groovy

--- second-job/Jenkinsfile.groovy
+++ second-job/Jenkinsfile.groovy
@@ -1,29 +1,27 @@
 pipeline {
-  agent none
-  stages {
-    stage('Back-end') {
-      agent {
-        docker { image 'maven:3.8.1-adoptopenjdk-11' }
-      }
-      steps {
-        script {
-            public class HelloWorld {
-                public static void main (String[] args) {
-                    System.out.println("Hello, World!!");
+    agent none
+    stages {
+        stage('Back-end') {
+            agent {
+                docker { image 'maven:3.8.1-adoptopenjdk-11' }
+            }
+            steps {
+                script {
+                    sh 'echo "public class HelloWorld { public static void main (String[] args) { System.out.println(\\"Hello, World!!\\"); } }" > HelloWorld.java'
+                    sh 'javac HelloWorld.java'
+                    sh 'java HelloWorld'
                 }
             }
         }
-      }
-    }
-    stage('Front-end') {
-      agent {
-        docker { image 'node:16-alpine' }
-      }
-      steps {
-        script {
-            console.log("Hello, World!")
+        stage('Front-end') {
+            agent {
+                docker { image 'node:16-alpine' }
+            }
+            steps {
+                script {
+                    echo "Hello, World!"
+                }
+            }
         }
-      }
     }
-  }
 }
