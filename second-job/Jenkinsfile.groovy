0edbd9bc1a392debf8185ba808015fee06d34469
Karthik <124011389+karthiksaki@users.noreply.github.com>
2023-08-24 21:03:14 -0500

    Update Jenkinsfile.groovy

--- second-job/Jenkinsfile.groovy
+++ second-job/Jenkinsfile.groovy
@@ -1,22 +1,21 @@
-pipeline{
-    agent none {
-        stages{
-            stage('stage-1'){
-                agent{
+pipeline {
+    agent none 
+        stages {
+            stage('stage-1') {
+                agent {
                 docker{ image 'maven:3.8.1-adoptopenjdk-11'}
                 }
             }
-            steps{
+            steps {
                 sh 'mvn--version'
             }
-            stage('stage-2'){
-                agent{
-                    docker{ image 'node:16-alpine' }
+            stage('stage-2') {
+                agent {
+                    docker { image 'node:16-alpine' }
                 }
-            steps{
+            steps {
                 sh 'node-version'
             }
             }
         }
-    }
 }
