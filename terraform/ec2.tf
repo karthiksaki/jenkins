af88390e8cfb954353927cf61cfdd896544e91c6
Karthik <124011389+karthiksaki@users.noreply.github.com>
2023-10-03 16:43:28 -0500

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -2,7 +2,7 @@ module "ec2_instance"{
 source  = "terraform-aws-modules/ec2-instance/aws"
 count = 3 
 ami               = data.aws_ami.amzlinux.id # Replace with your desired AMI ID
-key_name = "dallas"
+key_name = "texas"
 availability_zone = element(["us-west-1a", "us-west-1b", "us-west-1c"], count.index)
 tags = {
      Name = "Instance-${count.index}"
